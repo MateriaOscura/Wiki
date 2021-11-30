20519b6d78d7d5a1403ab89376bffdbccc6ff201
BR <benjaroa@gmail.com>
2021-11-30 02:47:00 -0300

    Edita wikiEntry

--- src/templates/WikiEntry.vue
+++ src/templates/WikiEntry.vue
@@ -1,10 +1,12 @@
 <template>
   <Layout>
-    <h1>{{ $page.wikiEntry.title }}</h1>
-    <p class="intro">
-      {{ $page.wikiEntry.excerpt }}
-    </p>
-    <VueRemarkContent />
+    <div class="mb-16 prose dark:prose-light lg:prose-lg xl:prose-xl">
+      <h1>{{ $page.wikiEntry.title }}</h1>
+      <p class="intro">
+        {{ $page.wikiEntry.excerpt }}
+      </p>
+      <VueRemarkContent />
+    </div>
   </Layout>
 </template>
 
